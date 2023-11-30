24e423c7a48fa0563d117b3c21ca6be7a86d2a11
sangeng7 <sangeng7@gmail.com>
2023-11-30 17:06:00 +0545

    loginUpdated

--- src/Login/Login.jsx
+++ src/Login/Login.jsx
@@ -7,23 +7,21 @@ function Login() {
             <div className="login">
                 <div className="container">
                     <div className="login-body">
-
                         <div className="row">
                             <div className="col-md-6 p-0">
                                 <div className="login-body-img">
-                                    <img className='w-100' src="https://eskil.qodeinteractive.com/wp-content/uploads/2022/03/h4-img-6.jpg" alt="" />
+                                    <img className='w-100 rounded-start-4' src="https://eskil.qodeinteractive.com/wp-content/uploads/2022/03/h4-img-6.jpg" alt="" />
                                 </div>
                             </div>
                             <div className="col-md-6 p-0">
                                 <div className="login-body-form d-flex align-items-center rounded-end-4">
-                                    <div className="">
+                                    <div className="form">
                                         <h3 className='mb-4'>Sign in to Account</h3>
                                         <input type="number" placeholder='Phone Number*' />
                                         <input type="password" placeholder='Password*' />
                                         <button className='form-btn btn btn-primary w-100'>Login <span><i class="fa fa-long-arrow-right" aria-hidden="true"></i></span></button>
                                         <p className='mt-4'>Don't have an account? <span><strong>Sign up</strong></span></p>
                                     </div>
-
                                 </div>
                             </div>
                         </div>
