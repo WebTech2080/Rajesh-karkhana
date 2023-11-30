09e592f98c3c818a716ae2243436480fd2275384
sachin shrestha <simonmaharjan2@gmail.com>
2023-11-30 12:42:17 +0545

    myone

---
+++ src/Gallary/Gallary.jsx
@@ -0,0 +1,75 @@
+import React from 'react'
+import "./gallary.scss"
+
+function Gallary() {
+    return (
+        <div>
+            <div className="wrapper container">
+                <div className="gallery row">
+                    <h1>rom rom bhaiyo</h1>
+                    <ul>
+                        <li>
+                            <div className="col-lg-3">
+                            <figure className="figure">
+                                <div className="figure-thumbnail">
+                                    <img src="https://images.unsplash.com/photo-1586023492125-27b2c045efd7?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8bW9kZXJuJTIwZnVybml0dXJlfGVufDB8fDB8fHww" />
+                                </div>
+                                <figcaption className="figure-caption">
+                                    <h2>Heading</h2>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, <a href="#">Link</a>
+                                </figcaption>
+                            </figure>
+                            </div>
+                         
+                        </li>
+                        <li>
+                        <div className="col-lg-3">
+                        <figure className="figure">
+                                <div className="figure-thumbnail">
+                                    <img src="https://media.istockphoto.com/id/1415799772/photo/home-interior-with-vintage-furniture.webp?b=1&s=170667a&w=0&k=20&c=7n4U6vnFtYrX-FE84s2GXIzqkBIkvPTAE2uG8mB8MLA=" />
+                                </div>
+                                <figcaption className="figure-caption">
+                                    <h2>Heading</h2>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, <a href="#">Link</a>
+                                </figcaption>
+                            </figure>
+                        </div>
+                           
+                        </li>
+                        <li>
+                            <div className="col-lg-3">
+                                 <figure className="figure">
+                                <div className="figure-thumbnail">
+                                    <img src="https://media.istockphoto.com/id/1415799772/photo/home-interior-with-vintage-furniture.webp?b=1&s=170667a&w=0&k=20&c=7n4U6vnFtYrX-FE84s2GXIzqkBIkvPTAE2uG8mB8MLA=" />
+                                </div>
+                                <figcaption className="figure-caption">
+                                    <h2>Heading</h2>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, <a href="#">Link</a>
+                                </figcaption>
+                            </figure>
+                            </div>
+                           
+                        </li>
+                        <li>
+                            <div className="col-lg-3">
+                                 <figure className="figure">
+                                <div className="figure-thumbnail">
+                                    <img src="https://media.istockphoto.com/id/1415799772/photo/home-interior-with-vintage-furniture.webp?b=1&s=170667a&w=0&k=20&c=7n4U6vnFtYrX-FE84s2GXIzqkBIkvPTAE2uG8mB8MLA=" />
+                                </div>
+                                <figcaption className="figure-caption">
+                                    <h2>Heading</h2>
+                                    Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, <a href="#">Link</a>
+                                </figcaption>
+                            </figure>
+                            </div>
+                           
+                        </li>
+                    </ul>
+                </div>
+            </div>
+
+        </div>
+    )
+}
+
+export default Gallary
