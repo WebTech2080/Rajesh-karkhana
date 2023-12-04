f0a101e2337fd32db343ef9ecc94f4a393cea75c
sangeng7 <sangeng7@gmail.com>
2023-12-04 17:02:49 +0545

    Slider Section Done!

--- src/Contact/Contact.jsx
+++ src/Contact/Contact.jsx
@@ -45,8 +45,7 @@ function Contact() {
             </div>
           </div>
           <div className="contact-map">
-<iframe className='w-100' src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d14130.948231973627!2d85.34462205!3d27.694520599999997!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1sen!2snp!4v1701326295712!5m2!1sen!2snp" width={600} height={450} style={{border: 0}} allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade" />
-            </div>
+          <div className="mapouter"><div className="gmap_canvas"><iframe src="https://maps.google.com/maps?q=Old%20baneshwor&t=&z=19&ie=UTF8&iwloc=&output=embed" frameBorder={0} scrolling="no" style={{ width: 1080, height: 400 }} /><style dangerouslySetInnerHTML={{ __html: ".mapouter{position:relative;height:400px;width:1080px;background:#fff;} .maprouter a{color:#fff !important;position:absolute !important;top:0 !important;z-index:0 !important;}" }} /><a href="https://blooketjoin.org">blooketjoin</a><style dangerouslySetInnerHTML={{ __html: ".gmap_canvas{overflow:hidden;height:400px;width:1080px}.gmap_canvas iframe{position:relative;z-index:2}" }} /></div></div>            </div>
 
         </div>
       </div>
--- src/Featured/Featured.jsx
+++ src/Featured/Featured.jsx
@@ -4,7 +4,7 @@ import './featured.scss'
 function Featured() {
     return (
         <>
-            <div className="featured my-5">
+            <div className="featured my-5  py-5">
                 <div className="container">
                     <div className="featured-header py-4 text-center">
                         <h2>Featured <span>Products</span></h2>
@@ -12,12 +12,76 @@ function Featured() {
                     <div className="featured-items pt-4">
                         <div className="featured-items-card">
                             <div className="owl-carousel">
-                                <div className="item">
+                                <div className="item overflow-hidden">
                                     <div className="featured-items-card-img">
-                                        <img className='w-100' src="https://decorazzio.cmsmasters.net/wp-content/uploads/2015/05/shop_22-1.jpg" alt="" />
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-2.jpg" />
                                     </div>
-                                    <div className="featured-items-card-text">
-                                        <h5></h5>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Get Cozy With A Touch of Elegance </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
+                                    </div>
+                                </div>
+                                <div className="item overflow-hidden">
+                                    <div className="featured-items-card-img">
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-2.jpg" />
+                                    </div>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Top Furniture Catlogue Design  </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
+                                    </div>
+                                </div>
+                                <div className="item overflow-hidden">
+                                    <div className="featured-items-card-img">
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-3.jpg" />
+                                    </div>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Read About New Design Features  </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
+                                    </div>
+                                </div>
+                                <div className="item overflow-hidden">
+                                    <div className="featured-items-card-img">
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-4.jpg" />
+                                    </div>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Stylish Bedroom Design Details  </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
+                                    </div>
+                                </div>
+                                <div className="item overflow-hidden">
+                                    <div className="featured-items-card-img">
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-3.jpg" />
+                                    </div>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Get Cozy With A Touch of Elegance </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
+                                    </div>
+                                </div>
+                                <div className="item overflow-hidden">
+                                    <div className="featured-items-card-img">
+                                        <img className='w-100' src="https://tobel.qodeinteractive.com/wp-content/uploads/2021/05/Blog-list-img-2.jpg" />
+                                    </div>
+
+                                    <div className="featured-items-card-text py-3">
+                                        <p>May 24, 2021 - By Jessy J. -Decor</p>
+                                        <h3 className='my-2'>Get Cozy With A Touch of Elegance </h3>
+                                        <a className='mt-2' href="">Read More</a>
+                                        
                                     </div>
                                 </div>
                             </div>
